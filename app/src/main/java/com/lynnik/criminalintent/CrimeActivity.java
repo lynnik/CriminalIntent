289d57d2c10e00212a342da048cd89c7ed37feb1
Lynnik Vlad <lynnik.v@gmail.com>
2017-07-04 13:45:49 +0300

    Fixed a bug where the CrimeFragment was not displayed.

--- app/src/main/java/com/lynnik/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/lynnik/criminalintent/CrimeActivity.java
@@ -15,7 +15,9 @@ public class CrimeActivity extends FragmentActivity {
     FragmentManager fm = getSupportFragmentManager();
     Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
 
-    if (fragment != null) {
+    // в книге было так, но экран пустой
+    //if (fragment != null) {
+    if (fragment == null) {
       fragment = new CrimeFragment();
       fm.beginTransaction()
           .add(R.id.fragmentContainer, fragment)
--- app/src/main/java/com/lynnik/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/lynnik/criminalintent/CrimeFragment.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -55,7 +56,8 @@ public class CrimeFragment extends Fragment {
     });
 
     mDateButton = (Button) v.findViewById(R.id.crime_date);
-    mDateButton.setText(mCrime.getDate().toString());
+    //mDateButton.setText(mCrime.getDate().toString());
+    mDateButton.setText(DateFormat.format("E, M, dd, yyyy", mCrime.getDate()));
     mDateButton.setEnabled(false);
 
     mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
