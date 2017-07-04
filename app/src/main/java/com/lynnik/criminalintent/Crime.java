32aadf063e6ee91d127bd4f0c0540e61ba7de1da
Lynnik Vlad <lynnik.v@gmail.com>
2017-07-04 12:29:57 +0300

    Crime class updated.

--- app/src/main/java/com/lynnik/criminalintent/Crime.java
+++ app/src/main/java/com/lynnik/criminalintent/Crime.java
@@ -1,14 +1,18 @@
 package com.lynnik.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class Crime {
 
   private UUID mId;
   private String mTitle;
+  private Date mDate;
+  private boolean mSolved;
 
   public Crime() {
     mId = UUID.randomUUID();
+    mDate = new Date();
   }
 
   public UUID getId() {
@@ -21,5 +25,21 @@ public class Crime {
 
   public void setTitle(String title) {
     mTitle = title;
+  }
+
+  public Date getDate() {
+    return mDate;
+  }
+
+  public void setDate(Date date) {
+    mDate = date;
+  }
+
+  public boolean isSolved() {
+    return mSolved;
+  }
+
+  public void setSolved(boolean solved) {
+    mSolved = solved;
   }
 }
