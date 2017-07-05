f266be941474176deb5369b4c4dbcd05cc606d77
Lynnik Vlad <lynnik.v@gmail.com>
2017-07-05 13:25:31 +0300

    Created a class and a fragment of CrimeList.

---
+++ app/src/main/java/com/lynnik/criminalintent/CrimeListActivity.java
@@ -0,0 +1,11 @@
+package com.lynnik.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+public class CrimeListActivity extends SingleFragmentActivity {
+
+  @Override
+  protected Fragment createFragment() {
+    return new CrimeListFragment();
+  }
+}
---
+++ app/src/main/java/com/lynnik/criminalintent/CrimeListFragment.java
@@ -0,0 +1,7 @@
+package com.lynnik.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+public class CrimeListFragment extends Fragment {
+
+}
