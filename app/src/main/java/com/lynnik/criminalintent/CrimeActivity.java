f185608214dff36417204730bd0a47651a9b58a2
Lynnik Vlad <lynnik.v@gmail.com>
2017-07-05 13:06:03 +0300

    The CrimeActivity layout name has been changed.

--- app/src/main/java/com/lynnik/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/lynnik/criminalintent/CrimeActivity.java
@@ -10,7 +10,7 @@ public class CrimeActivity extends FragmentActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_crime);
+    setContentView(R.layout.activity_fragment);
 
     FragmentManager fm = getSupportFragmentManager();
     Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
