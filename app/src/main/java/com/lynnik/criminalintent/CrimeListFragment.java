d101c39b1fb613cfcc656b304b1904fb04ecc00a
Lynnik Vlad <lynnik.v@gmail.com>
2017-07-05 13:50:18 +0300

    The class CrimeListFragment was changed.

--- app/src/main/java/com/lynnik/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/lynnik/criminalintent/CrimeListFragment.java
@@ -1,7 +1,28 @@
 package com.lynnik.criminalintent;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class CrimeListFragment extends Fragment {
 
+  private RecyclerView mCrimeRecyclerView;
+
+  @Nullable
+  @Override
+  public View onCreateView(
+      LayoutInflater inflater,
+      @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState) {
+    View v = inflater.inflate(R.layout.fragment_crime_list, container, false);
+    mCrimeRecyclerView = (RecyclerView) v.findViewById(R.id.crime_recycler_view);
+    mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+    return v;
+  }
 }
