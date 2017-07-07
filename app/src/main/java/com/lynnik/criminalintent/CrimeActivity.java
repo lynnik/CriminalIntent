943072e00e84f0d22987111f8f13bfc6b037d626
Lynnik Vlad <lynnik.v@gmail.com>
2017-07-07 09:13:15 +0300

    Transferring data to CrimeFragment using fragment arguments.

--- app/src/main/java/com/lynnik/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/lynnik/criminalintent/CrimeActivity.java
@@ -8,7 +8,7 @@ import java.util.UUID;
 
 public class CrimeActivity extends SingleFragmentActivity {
 
-  public static final String EXTRA_CRIME_ID =
+  private static final String EXTRA_CRIME_ID =
       "com.lynnik.criminalintent.crime_id";
 
   public static Intent newIntent(Context packageContext, UUID crimeId) {
@@ -19,6 +19,7 @@ public class CrimeActivity extends SingleFragmentActivity {
 
   @Override
   protected Fragment createFragment() {
-    return new CrimeFragment();
+    UUID crimeId = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
+    return CrimeFragment.newInstance(crimeId);
   }
 }
