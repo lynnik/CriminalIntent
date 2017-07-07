a95da0aa159103893f0ea17cc71c4fd83a2e95e6
Lynnik Vlad <lynnik.v@gmail.com>
2017-07-07 09:00:47 +0300

    Fixed CrimeActivity launch error.

--- app/src/main/java/com/lynnik/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/lynnik/criminalintent/CrimeListFragment.java
@@ -70,7 +70,7 @@ public class CrimeListFragment extends Fragment {
 
     @Override
     public void onClick(View view) {
-      Intent intent = new Intent(getActivity(), CrimeActivity.class);
+      Intent intent = CrimeActivity.newIntent(getActivity(), mCrime.getId());
       startActivity(intent);
     }
   }
