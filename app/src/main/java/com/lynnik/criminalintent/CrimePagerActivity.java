e92e08caba44fd9708d9221f92f46ae66b6e5581
Lynnik Vlad <lynnik.v@gmail.com>
2017-07-08 18:37:52 +0300

    Created CrimePagerActivity class and its layout.

---
+++ app/src/main/java/com/lynnik/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,13 @@
+package com.lynnik.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class CrimePagerActivity extends FragmentActivity {
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_crime_pager);
+  }
+}
