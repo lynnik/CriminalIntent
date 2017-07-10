54923a1d893e1643b5fcde9079fa7d544b8237fa
Lynnik Vlad <lynnik.v@gmail.com>
2017-07-10 16:55:56 +0300

    Implemented display DialogFragment.

--- app/src/main/java/com/lynnik/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/lynnik/criminalintent/CrimeFragment.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.text.format.DateFormat;
@@ -20,6 +21,7 @@ import java.util.UUID;
 public class CrimeFragment extends Fragment {
 
   private static final String ARG_CRIME_ID = "crime_id";
+  private static final String DIALOG_DATE = "DialogDate";
 
   private Crime mCrime;
   private EditText mTitleField;
@@ -74,7 +76,14 @@ public class CrimeFragment extends Fragment {
     mDateButton = (Button) v.findViewById(R.id.crime_date);
     mDateButton.setText(
         DateFormat.format("EEEE, dd MMMM, yyyy", mCrime.getDate()));
-    mDateButton.setEnabled(false);
+    mDateButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        FragmentManager manager = getFragmentManager();
+        DatePickerFragment dialog = new DatePickerFragment();
+        dialog.show(manager, DIALOG_DATE);
+      }
+    });
 
     mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
     mSolvedCheckBox.setChecked(mCrime.isSolved());
--- app/src/main/java/com/lynnik/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/lynnik/criminalintent/DatePickerFragment.java
@@ -1,7 +1,7 @@
 package com.lynnik.criminalintent;
 
 import android.app.Dialog;
-import android.app.DialogFragment;
+import android.support.v4.app.DialogFragment;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 
