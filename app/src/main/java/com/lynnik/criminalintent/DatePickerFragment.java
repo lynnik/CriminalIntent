f5106ff263d70b655a7a22baa7fdc94fcc8e59f8
Lynnik Vlad <lynnik.v@gmail.com>
2017-07-10 16:24:43 +0300

    Created new DatePickerFragment class.

---
+++ app/src/main/java/com/lynnik/criminalintent/DatePickerFragment.java
@@ -0,0 +1,17 @@
+package com.lynnik.criminalintent;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+
+public class DatePickerFragment extends DialogFragment {
+
+  @Override
+  public Dialog onCreateDialog(Bundle savedInstanceState) {
+    return new AlertDialog.Builder(getActivity())
+        .setTitle(R.string.date_picker_title)
+        .setPositiveButton(android.R.string.ok, null)
+        .create();
+  }
+}
