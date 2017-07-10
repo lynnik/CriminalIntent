045ba969f36ff02908e279f0271100b9e2d31d6a
Lynnik Vlad <lynnik.v@gmail.com>
2017-07-10 17:58:14 +0300

    Implementrd the transfer of data to the target fragment.

--- app/src/main/java/com/lynnik/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/lynnik/criminalintent/CrimeFragment.java
@@ -1,6 +1,7 @@
 package com.lynnik.criminalintent;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -16,12 +17,15 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class CrimeFragment extends Fragment {
 
   private static final String ARG_CRIME_ID = "crime_id";
   private static final String DIALOG_DATE = "DialogDate";
+
+  private static final int REQUEST_DATE = 0;
 
   private Crime mCrime;
   private EditText mTitleField;
@@ -74,14 +78,14 @@ public class CrimeFragment extends Fragment {
     });
 
     mDateButton = (Button) v.findViewById(R.id.crime_date);
-    mDateButton.setText(
-        DateFormat.format("EEEE, dd MMMM, yyyy", mCrime.getDate()));
+    updateDate();
     mDateButton.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
         FragmentManager manager = getFragmentManager();
         DatePickerFragment dialog = DatePickerFragment
             .newInstance(mCrime.getDate());
+        dialog.setTargetFragment(CrimeFragment.this, REQUEST_DATE);
         dialog.show(manager, DIALOG_DATE);
       }
     });
@@ -100,7 +104,25 @@ public class CrimeFragment extends Fragment {
     return v;
   }
 
+  @Override
+  public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    if (resultCode != Activity.RESULT_OK)
+      return;
+
+    if (requestCode == REQUEST_DATE) {
+      Date date = (Date) data
+          .getSerializableExtra(DatePickerFragment.EXTRA_DATE);
+      mCrime.setDate(date);
+      updateDate();
+    }
+  }
+
   public void returnResult() {
     getActivity().setResult(Activity.RESULT_OK, null);
+  }
+
+  private void updateDate() {
+    mDateButton.setText(
+        DateFormat.format("EEEE, dd MMMM, yyyy", mCrime.getDate()));
   }
 }
--- app/src/main/java/com/lynnik/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/lynnik/criminalintent/DatePickerFragment.java
@@ -1,6 +1,9 @@
 package com.lynnik.criminalintent;
 
+import android.app.Activity;
 import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
@@ -10,9 +13,11 @@ import android.widget.DatePicker;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class DatePickerFragment extends DialogFragment {
 
+  public static final String EXTRA_DATE = "com.lynnik.criminalintent.date";
   private static final String ARG_DATE = "date";
 
   private DatePicker mDatePicker;
@@ -45,7 +50,28 @@ public class DatePickerFragment extends DialogFragment {
     return new AlertDialog.Builder(getActivity())
         .setView(v)
         .setTitle(R.string.date_picker_title)
-        .setPositiveButton(android.R.string.ok, null)
+        .setPositiveButton(android.R.string.ok,
+            new DialogInterface.OnClickListener() {
+              @Override
+              public void onClick(DialogInterface dialogInterface, int i) {
+                int year = mDatePicker.getYear();
+                int month = mDatePicker.getMonth();
+                int day = mDatePicker.getDayOfMonth();
+                Date date = new GregorianCalendar(year, month, day).getTime();
+                sendResult(Activity.RESULT_OK, date);
+              }
+            })
         .create();
+  }
+
+  private void sendResult(int resultCode, Date date) {
+    if (getTargetFragment() == null)
+      return;
+
+    Intent intent = new Intent();
+    intent.putExtra(EXTRA_DATE, date);
+
+    getTargetFragment()
+        .onActivityResult(getTargetRequestCode(), resultCode, intent);
   }
 }
