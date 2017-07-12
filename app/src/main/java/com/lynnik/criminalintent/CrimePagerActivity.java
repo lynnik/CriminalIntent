7a94d2a52685c29529235f309f1ac121a8c278b8
Lynnik Vlad <lynnik.v@gmail.com>
2017-07-12 13:22:53 +0300

    Added transition to AppCompatActivity.

--- app/src/main/java/com/lynnik/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/lynnik/criminalintent/CrimePagerActivity.java
@@ -8,11 +8,12 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
 import java.util.List;
 import java.util.UUID;
 
-public class CrimePagerActivity extends FragmentActivity {
+public class CrimePagerActivity extends AppCompatActivity {
 
   public static final String EXTRA_CRIME_ID =
       "com.lynnik.criminalintent.crime_id";
