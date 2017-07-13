8328be8ab7c2001308ebd837037eac43b5464743
Lynnik Vlad <lynnik.v@gmail.com>
2017-07-13 15:39:08 +0300

    Preparing the model for workong with the database.

--- app/src/main/java/com/lynnik/criminalintent/CrimeLab.java
+++ app/src/main/java/com/lynnik/criminalintent/CrimeLab.java
@@ -5,21 +5,18 @@ import android.database.sqlite.SQLiteDatabase;
 
 import com.lynnik.criminalintent.database.CrimeBaseHelper;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 public class CrimeLab {
 
   private static CrimeLab sCrimeLab;
-  private List<Crime> mCrimes;
   private Context mContext;
   private SQLiteDatabase mDatabase;
 
   private CrimeLab(Context context) {
     mContext = context.getApplicationContext();
     mDatabase = new CrimeBaseHelper(mContext).getWritableDatabase();
-    mCrimes = new ArrayList<>();
   }
 
   public static CrimeLab getInstance(Context context) {
@@ -29,26 +26,20 @@ public class CrimeLab {
   }
 
   public void addCrime(Crime c) {
-    mCrimes.add(c);
   }
 
   public void removeCrime(Crime c) {
-    mCrimes.remove(c);
   }
 
   public List<Crime> getCrimes() {
-    return mCrimes;
+    return null;
   }
 
   public Crime getCrime(UUID id) {
-    for (Crime crime : mCrimes) {
-      if (crime.getId().equals(id))
-        return crime;
-    }
     return null;
   }
 
   public boolean isEmpty() {
-    return mCrimes.size() == 0;
+    return false;
   }
 }
--- app/src/main/java/com/lynnik/criminalintent/database/CrimeBaseHelper.java
+++ app/src/main/java/com/lynnik/criminalintent/database/CrimeBaseHelper.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.lynnik.criminalintent.database.CrimeDbSchema.CrimeTable;
+
 public class CrimeBaseHelper extends SQLiteOpenHelper {
 
   private static final int VERSION = 1;
@@ -15,7 +17,14 @@ public class CrimeBaseHelper extends SQLiteOpenHelper {
 
   @Override
   public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+    sqLiteDatabase.execSQL("create table " + CrimeTable.NAME + "(" +
+        " _id integer primary key autoincrement, " +
+        CrimeTable.Cols.UUID + ", " +
+        CrimeTable.Cols.TITLE + ", " +
+        CrimeTable.Cols.DATE + ", " +
+        CrimeTable.Cols.SOLVED +
+        ")"
+    );
   }
 
   @Override
