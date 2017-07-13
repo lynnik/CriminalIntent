5406231f2173d569e138bd9109e78af19e319c76
Lynnik Vlad <lynnik.v@gmail.com>
2017-07-13 15:28:04 +0300

    Implemented the opening of a database.

--- app/src/main/java/com/lynnik/criminalintent/CrimeLab.java
+++ app/src/main/java/com/lynnik/criminalintent/CrimeLab.java
@@ -1,6 +1,9 @@
 package com.lynnik.criminalintent;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.lynnik.criminalintent.database.CrimeBaseHelper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,8 +13,12 @@ public class CrimeLab {
 
   private static CrimeLab sCrimeLab;
   private List<Crime> mCrimes;
+  private Context mContext;
+  private SQLiteDatabase mDatabase;
 
   private CrimeLab(Context context) {
+    mContext = context.getApplicationContext();
+    mDatabase = new CrimeBaseHelper(mContext).getWritableDatabase();
     mCrimes = new ArrayList<>();
   }
 
