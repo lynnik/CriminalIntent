c2e306290ef775b1c9fabf3f278f8d207f3ee460
Lynnik Vlad <lynnik.v@gmail.com>
2017-07-13 16:58:59 +0300

    Added update of model data.

--- app/src/main/java/com/lynnik/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/lynnik/criminalintent/CrimeListFragment.java
@@ -129,6 +129,7 @@ public class CrimeListFragment extends Fragment {
       mAdapter = new CrimeAdapter(crimes);
       mCrimeRecyclerView.setAdapter(mAdapter);
     } else {
+      mAdapter.setCrimes(crimes);
       mAdapter.notifyItemChanged(mCrimePosition);
     }
 
@@ -207,6 +208,10 @@ public class CrimeListFragment extends Fragment {
     @Override
     public int getItemCount() {
       return mCrimes.size();
+    }
+
+    public void setCrimes(List<Crime> crimes) {
+      mCrimes = crimes;
     }
   }
 }
