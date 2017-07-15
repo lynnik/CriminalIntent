c1d1a56fdd1aca70e59159cf4db2a4cda28bec97
Lynnik Vlad <lynnik.v@gmail.com>
2017-07-15 12:27:30 +0300

    Changed layouts for CrimeFragment.

--- app/src/main/java/com/lynnik/criminalintent/CrimeLab.java
+++ app/src/main/java/com/lynnik/criminalintent/CrimeLab.java
@@ -61,7 +61,8 @@ public class CrimeLab {
   }
 
   public void removeCrime(Crime c) {
-
+    mDatabase.delete(CrimeTable.NAME,
+        CrimeTable.Cols.UUID + " = ?", new String[]{c.getId().toString()});
   }
 
   public List<Crime> getCrimes() {
