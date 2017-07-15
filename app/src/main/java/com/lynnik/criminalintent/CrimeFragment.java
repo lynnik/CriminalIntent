c893cbcdb9dca8ec421fa8340c0fd9f44a7c5c70
Lynnik Vlad <lynnik.v@gmail.com>
2017-07-15 13:33:32 +0300

    Implemented to obtain data from the contact list.

--- app/src/main/java/com/lynnik/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/lynnik/criminalintent/CrimeFragment.java
@@ -2,6 +2,8 @@ package com.lynnik.criminalintent;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
@@ -174,6 +176,24 @@ public class CrimeFragment extends Fragment {
           .getSerializableExtra(DatePickerFragment.EXTRA_DATE);
       mCrime.setDate(date);
       updateDate();
+    } else if (requestCode == REQUEST_CONTACT && data != null) {
+      Uri contactUri = data.getData();
+      String[] queryFields = new String[]{
+          ContactsContract.Contacts.DISPLAY_NAME
+      };
+      Cursor c = getActivity().getContentResolver().
+          query(contactUri, queryFields, null, null, null);
+
+      try {
+        if (c.getCount() == 0)
+          return;
+        c.moveToFirst();
+        String suspect = c.getString(0);
+        mCrime.setSuspect(suspect);
+        mSuspectButton.setText(suspect);
+      } finally {
+        c.close();
+      }
     }
   }
 
