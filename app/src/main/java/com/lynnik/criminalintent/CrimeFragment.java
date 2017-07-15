8301fb0c9a07c171230d794d31ebf03caff0c24b
Lynnik Vlad <lynnik.v@gmail.com>
2017-07-15 13:19:28 +0300

    Contact request was implemented.

--- app/src/main/java/com/lynnik/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/lynnik/criminalintent/CrimeFragment.java
@@ -3,6 +3,7 @@ package com.lynnik.criminalintent;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -29,12 +30,14 @@ public class CrimeFragment extends Fragment {
   private static final String DIALOG_DATE = "DialogDate";
 
   private static final int REQUEST_DATE = 0;
+  private static final int REQUEST_CONTACT = 1;
 
   private Crime mCrime;
   private EditText mTitleField;
   private Button mDateButton;
   private CheckBox mSolvedCheckBox;
   private Button mReportButton;
+  private Button mSuspectButton;
 
   public static CrimeFragment newInstance(UUID crimeId) {
     Bundle args = new Bundle();
@@ -126,6 +129,19 @@ public class CrimeFragment extends Fragment {
         startActivity(i);
       }
     });
+
+    final Intent pickContact = new Intent(Intent.ACTION_PICK,
+        ContactsContract.Contacts.CONTENT_URI);
+    mSuspectButton = (Button) v.findViewById(R.id.crime_suspect);
+    mSuspectButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        startActivityForResult(pickContact, REQUEST_CONTACT);
+      }
+    });
+
+    if (mCrime.getSuspect() != null)
+      mSuspectButton.setText(mCrime.getSuspect());
 
     return v;
   }
