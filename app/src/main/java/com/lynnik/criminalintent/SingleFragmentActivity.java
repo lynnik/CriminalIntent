15b197b783b69ff51f618eaff6daac08440b8de6
Lynnik Vlad <lynnik.v@gmail.com>
2017-07-17 12:12:47 +0300

    Am layut with two views of the fragments was created.

--- app/src/main/java/com/lynnik/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/lynnik/criminalintent/SingleFragmentActivity.java
@@ -1,6 +1,7 @@
 package com.lynnik.criminalintent;
 
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
@@ -9,10 +10,15 @@ public abstract class SingleFragmentActivity extends AppCompatActivity {
 
   protected abstract Fragment createFragment();
 
+  @LayoutRes
+  protected int getLayoutResId() {
+    return R.layout.activity_fragment;
+  }
+
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_fragment);
+    setContentView(getLayoutResId());
 
     FragmentManager fm = getSupportFragmentManager();
     Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
